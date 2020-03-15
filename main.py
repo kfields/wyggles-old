86a0ea6b1ec890702643f7823c1c0d1f48dfb77c
Kurtis Fields <kurtisfields@gmail.com>
2020-03-15 18:19:40 -0400

    eliminate lib directory

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-#! /usr/bin/env python
-
 import sys
 import os
 
-try:
-    __file__
-except NameError:
-    pass
-else:
-    libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'lib'))
-    sys.path.insert(0, libdir)
+sys.path.insert(0, os.path.abspath(__file__))
 
 import data
 import math
